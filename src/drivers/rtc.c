2c98340ed1863d2ced77985f6c15b53679441a70
dmipin02 <dmipin02@gmail.com>
2020-04-23 01:31:06 +0300

    LICENSING

--- src/drivers/rtc.c
+++ src/drivers/rtc.c
@@ -1,3 +1,22 @@
+/*
+* This file is part of the ChinaWatch OS project.
+*
+* Copyright (C) 2020 ChinaWatch OS
+* Copyright (C) 2019 AJScorp
+*
+* This program is free software; you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation; version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with this program; if not, write to the Free Software 
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
+*/
 #include <drivers/rtc.h>
 
 static const uint8_t DayInMonth[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
--- src/exchandlers.s
+++ src/exchandlers.s
@@ -1,11 +1,12 @@
 /*
-* This file is part of the DZ09 project.
+* This file is part of the ChinaWatch OS project.
 *
+* Copyright (C) 2020 ChinaWatch OS
 * Copyright (C) 2019 AJScorp
 *
 * This program is free software; you can redistribute it and/or modify 
 * it under the terms of the GNU General Public License as published by 
-* the Free Software Foundation; version 2 of the License.
+* the Free Software Foundation; version 3 of the License.
 *
 * This program is distributed in the hope that it will be useful, 
 * but WITHOUT ANY WARRANTY; without even the implied warranty of 
